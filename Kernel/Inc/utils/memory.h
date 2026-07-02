78f3ab8e9e4363f72202640f10a2f09aefec15cf
Andrea Cipriani <andrea254cipriani@icloud.com>
2026-07-02 09:56:36 +0200

    Add _sbrk
    Kinda useless for it current state of development, but useful in future

--- Kernel/Inc/utils/memory.h
+++ Kernel/Inc/utils/memory.h
@@ -6,10 +6,12 @@
 #define MEMORY_H
 
 #include <stdint.h>
+#include <stddef.h>
 
-extern uint32_t _sheap;
-extern uint32_t _endOfRAM;
+extern uint8_t _sheap;                                     //Where heap starts
+extern uint8_t _endOfRAM;                                  //Where SRAM ends and stack stats
+extern uint8_t _min_stack_size;
 
-int _sbrk(int incr);
+void *_sbrk(int incr);
 
 #endif //MEMORY_H
--- Kernel/Src/utils/memory.c
+++ Kernel/Src/utils/memory.c
@@ -4,7 +4,19 @@
 
 #include "utils/memory.h"
 
-int _sbrk(int incr) {
+void *_sbrk(ptrdiff_t incr) {
+    const uint32_t limitStack = (uint32_t)&_endOfRAM - (uint32_t)&_min_stack_size;  //Stack limit, where it starts minus its minimal length
+    const uint8_t* max_heap = (uint8_t*)limitStack;             //Heap limit, wher it meets the stack
 
-    return 0;
+    static uint8_t *_sbrk_heap_end = NULL;                     //Pointer to current heap end
+    uint8_t *prev_heap_end;
+
+    if (_sbrk_heap_end == NULL) _sbrk_heap_end = &_sheap;       //Initialize where current heap ends (at heap starting point)
+
+    if (_sbrk_heap_end + incr > max_heap) return (void *)-1;    //Check if I'm overflowing the heap
+
+    prev_heap_end = _sbrk_heap_end;                             //Store previous value (used by the calling function to point data)
+    _sbrk_heap_end += incr;                                     //Increment current heap size
+
+    return (void *)prev_heap_end;                               //Return the pointer
 }
